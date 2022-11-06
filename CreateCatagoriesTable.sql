09c3b7a716aa067e00aac862347d2781ba621331
00012864 <72963327+00012864@users.noreply.github.com>
2022-11-06 23:39:57 +0500

    SQL to create categories table & add list of categories

---
+++ CreateCatagoriesTable.sql
@@ -0,0 +1,20 @@
+Create Database Catalog
+GO
+
+use Catalog
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Categories](
+[Id] [int] NOT NULL,
+[Name] [nvarchar](50) NULL,
+[Description] [nvarchar](50) NULL,
+CONSTRAINT [PK_Category] PRIMARY KEY CLUSTERED
+(
+[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = ON,
+ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
---
+++ InsertCategories - Copy.sql
@@ -0,0 +1,35 @@
+USE [Catalog]
+GO
+
+INSERT INTO [dbo].[categoriesCSV]
+           ([Id]
+           ,[Name]
+           ,[Description])
+     VALUES
+           ( 1
+           , 'Shoes'
+           ,'Tennis shoes, boots and more comfortable shoes'),
+		   
+		   ( 2
+		   , 'Clothing'
+		   , 'Jackets, sweaters, kids clothing and more'),
+
+		   ( 3
+		   , 'Bags'
+		   , 'Good quality bags, designer bags included'),
+
+		   ( 4
+		   , 'Electronics'
+		   , 'Phones, chargers and more electronics'), 
+
+		   ( 5
+		   , 'Skincare'
+		   , 'Creams, face wash and more'),
+
+		   ( 6 
+		   , 'Kitchen Equipment' 
+		   , 'Pots, pans, bowls and more');
+		   
+GO
+
+
